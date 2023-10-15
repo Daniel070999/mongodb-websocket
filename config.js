20e3193ecf427b54dc7b506cb6a52c6184aab206
Daniel070999 <danielixo99@gmail.com>
2023-10-14 21:21:51 -0500

    websocket

---
+++ config.js
@@ -0,0 +1,7 @@
+const pass = require('./clave');
+const config = {
+    PORT: 3000, 
+    DB_URL: `mongodb+srv://daniel:${pass.key}@cluster0.ydua0ua.mongodb.net/?retryWrites=true&w=majority`
+}
+
+module.exports = config
